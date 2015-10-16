08385a6b1531c5de62e550e38df4d41f4bfe9f93
tim-hub <tim_w@outlook.com>
2015-10-16 10:50:30 +0800

    use cube to replace the model, and change the input method

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
-
+	public float force=200f;
+	public float maxVelocity=2f;
 	private Animator anim;
 	private Rigidbody rb;
 
@@ -18,6 +19,9 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+#if Unity_Editor
+		//keyboard move
 		float h=Input.GetAxis("Horizontal");
 		float v=Input.GetAxis("Vertical");
 
@@ -29,9 +33,91 @@ public class PlayerMove : MonoBehaviour {
 
 
 		rb.MovePosition(transform.position+move);
+		//end key board move
+#endif
 
+		if(rb.velocity!=Vector3.zero){
+			Debug.Log(rb.velocity);
+		}
 
 
 
 	}
+
+
+	protected virtual void OnEnable()
+	{
+		// Hook into the OnSwipe event
+		Lean.LeanTouch.OnFingerSwipe += OnFingerSwipe;
+	}
+	
+	protected virtual void OnDisable()
+	{
+		// Unhook into the OnSwipe event
+		Lean.LeanTouch.OnFingerSwipe -= OnFingerSwipe;
+	}
+	
+	public void OnFingerSwipe(Lean.LeanFinger finger)
+	{
+		// Make sure the info text exists
+		if (gameObject != null)
+		{
+			// Store the swipe delta in a temp variable
+			var swipe = finger.SwipeDelta;
+			Debug.Log(swipe);
+//			if (swipe.x < -Mathf.Abs(swipe.y))
+//			{
+//
+//				
+//			}
+//			
+//			if (swipe.x > Mathf.Abs(swipe.y))
+//			{
+//
+//			}
+//			
+//			if (swipe.y < -Mathf.Abs(swipe.x))
+//			{
+//
+//			}
+//			
+//			if (swipe.y > Mathf.Abs(swipe.x))
+//			{
+//
+//			}
+			float h=swipe.x/100f;
+			float v=swipe.y/100f;
+
+
+
+
+			rb.AddForce(-v*force,0,h*force);
+
+//			if(swipe.x<0f){
+//				Debug.Log("Move back");
+//				rb.AddForce(0,0,-force));
+//				//rb.MovePosition(transform.position+new Vector3(0f,0f,-5f));
+//
+//			}
+//			if(swipe.x>0f){
+//				Debug.Log("Move back");
+//				rb.AddForce(0,0,force);
+//				//rb.MovePosition(transform.position+new Vector3(0f,0f,-5f));
+//				
+//			}
+//			if(swipe.x>0f){
+//				Debug.Log("Move forward");
+//				rb.AddForce(0,0,200);
+//			}
+//
+//			if(swipe.y<0f){
+//				rb.AddForce(200,0,0);
+//			}
+//			if(swipe.y>0f){
+//
+//				rb.AddForce(-200,0,0);
+//			}
+		}
+	}
 }
+
