5b3f9aec901ec133c9d577a5eb4140ed94ca4d68
tim-hub <tim_w@outlook.com>
2015-10-17 11:56:44 +0800

    add minimap

--- Assets/_Scripts/RayTest.cs
+++ Assets/_Scripts/RayTest.cs
@@ -4,13 +4,55 @@ using System.Collections;
 public class RayTest : MonoBehaviour {
 	public Camera cam;
 	public LayerMask field;
+
+	
+	public Transform football;
+	//public Vector3 direction;
+	
+	private bool attachFootball =false;
+	private Animator anim;
+	private Rigidbody rb;
+	private CharacterController cc;
+	private NavMeshAgent nma;
+	
+	
 	// Use this for initialization
 	void Start () {
-	
-	}
+		
+		anim=GetComponent<Animator>();
+		rb=GetComponent<Rigidbody>();
+		cc=GetComponent<CharacterController>();
+		nma=GetComponent<NavMeshAgent>();
+		
+	}	
 	
 	// Update is called once per frame
 	void Update () {
+
+
+#if UNITY_EDITOR
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("lift click");
+			Ray ray=cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			
+			
+			if(Physics.Raycast(ray,out hit,1000f, field)){
+
+				Debug.Log(hit.point);
+				//cc.SimpleMove(hit.point+new Vector3(0f,transform.position.y,0f));
+				nma.SetDestination(hit.point);
+				
+			}
+
+		}
+
+
+#endif
+
+
+
 		if(Input.touchCount>=1){
 			Debug.Log("touch");
 
@@ -21,8 +63,8 @@ public class RayTest : MonoBehaviour {
 
 
 			if(Physics.Raycast(ray,out hit,1000f, field)){
-				transform.position=hit.point;
-				
+
+				cc.SimpleMove(hit.point);
 				
 			}
 
